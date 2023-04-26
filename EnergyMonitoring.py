df25859208f26938f0c54254fd36d8bd4107e043
MatthisHoules <matthis.houles@imt-atlantique.net>
2023-04-26 10:20:51 +0200

    requests

--- EnergyMonitoring.py
+++ EnergyMonitoring.py
@@ -1,4 +1,9 @@
 from joulehunter import Profiler, renderers
+import pymongo
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mongo_db = mongo_client["energy_monitoring"]
+monitoring_col = mongo_db["energy"]
 
 class EnergyMonitoring(object):
     """_summary_
@@ -7,17 +12,19 @@ class EnergyMonitoring(object):
         object (_type_): _description_
     """
     
-    def __init__(self, app, *args, **kwargs):
+    def __init__(self, endpoint_name : str, endpoint_url : str, n_run_endpoint : int = 10, *args, **kwargs):
         """_summary_
         """
-        
-        self._app = app
-        
+
+        self._endpoint_name : str = endpoint_name
+        self._endpoint_url : str = endpoint_url   
+        self._n_run_endpoint : int = n_run_endpoint 
+        self._args = args
         self._kwargs = kwargs
-    # def __init__(self, *args, **kwargs)
+    # def __init__(self, endpoint_name : str, endpoint_url : str, *args, **kwargs)
 
     def __call__(self, obj):
-        """__call__
+        """_summary_
 
         Args:
             obj (_type_): _description_
@@ -27,15 +34,31 @@ class EnergyMonitoring(object):
         keys, values = zip(*self._kwargs.items())
         permutations_dicts = [dict(zip(keys, v)) for v in itertools.product(*values)]
 
-        for v in permutations_dicts : 
-            for i in range(1) :
-                
-                print(v)
+        for v in permutations_dicts:
+            mean_joules : float = 0.0
+            
+            for _ in range(self._n_run_endpoint) :
                 with Profiler(interval=0.0001) as p:
                     obj(**v)
-
-                # print(p.output(
-                #     renderers.JSONRenderer(show_all=True)
-                # ))
+                    
+                frame = p._get_last_session_or_fail().root_frame()
+                if frame:
+                    mean_joules += frame.time()
+                else:
+                    mean_joules += 0.0
+                
                 del p
+                
+            mean_joules /= self._n_run_endpoint
+            print("\n"*5, self._endpoint_name, mean_joules, "\n"*5)
+                
+            monitoring_col.insert_one(
+                {
+                    "endpoint_name" : self._endpoint_name,
+                    "endpoint_url" : self._endpoint_url,
+                    "joules": mean_joules,
+                    **v
+                }
+            )
     # def __call__(self, obj)
+# class EnergyMonitoring(object)
--- serve.py
+++ serve.py
@@ -14,43 +14,34 @@ def fibonacci(n):
         return (fibonacci(n-1) + fibonacci(n-2))
 # def fibonacci(n)
 
+
+
 app = Flask(__name__)
 
 
-@EnergyMonitoring(app, n = [20, 15])
+@EnergyMonitoring("index", "/<int:n>", 20, n=[20, 15])
 @app.route('/<int:n>')
 def index(n : int):
     ct = fibonacci(n)
     
-    response = requests.get('http://127.0.0.1:9091/5') 
-    print(response)
-
     return json.dumps({"Fibanicci, n=20:" :  ct})
 # def index(n : int)
 
 
-@EnergyMonitoring(app, n = [10, 5])
-@app.route('/fibo/<int:n>')
+@EnergyMonitoring("fibo", "/fibo/<int:n>", 20, n=[10, 5])
+@app.route("/fibo/<int:n>")
 def fibo(n : int):
     ct = fibonacci(n)
-    
-    
     
     response = app.response_class(
         response=json.dumps({"Fibanicci, n=20:" :  ct}),
         status=300,
         mimetype='application/json'
     )
+    
     return response
 # def fibo(n : int)
 
 
-
-from requests_html import HTMLSession
-from wsgiadapter import WSGIAdapter
-
-s = HTMLSession()
-s.mount("http://test", WSGIAdapter(app))
-
-r = s.get("http://test/fibo/5")
-print(r.content)
+if __name__ == "__main__" :
+    app.run()
