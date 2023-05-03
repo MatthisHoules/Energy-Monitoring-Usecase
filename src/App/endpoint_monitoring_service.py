857a75905cf3746df0c4273bae155361fb7484e7
Paul <vautier.paul14350@gmail.com>
2023-05-03 16:36:48 +0200

    added neighbours call

--- src/App/endpoint_monitoring_service.py
+++ src/App/endpoint_monitoring_service.py
@@ -2,9 +2,14 @@ import logging
 from bloompy import CountingBloomFilter
 from interval import interval
 from joulehunter import Profiler
-
+import requests
+from interval import interval
 from .local_energy_data import LocalEnergyData
 
+def get_neighbour(url: str) -> tuple(str, interval):
+    res = requests.get(url).json()
+    return (url, interval([res.get("lower"), res.get("upper")]))
+    
 class EndpointMonitoringService:
     """Service monitoring the energy consumption of a single endpoint. 
     It also provides the tuning of given arguments
@@ -26,37 +31,41 @@ class EndpointMonitoringService:
     def monitor_function_call(self, objective : float, **args):
         profiler = Profiler()
         profiler.start()
+        
+        # TODO : Call function and mo
 
+        frame = profiler._get_last_session_or_fail().root_frame()
+        consumption = 0.0
+        if frame:
+            consumption = frame.time() # time is the consumption in joules. JouleHunter did not modify this name
 
         
-        frame = profiler._get_last_session_or_fail().root_frame()
-        if frame:
-            frame = frame.time()
-        else:
-            frame = 0.0
 
-    def process_arguments(self, objective : float, arguments: list[float]):
+    def process_arguments(self, objective : float, arguments: list[float]) -> list[float]:
         if objective < 0:
             logging.warn(f"{self._name} has not objective defined.")
             pass
         logging.info(f"{self._name} has an objective of {objective}")
 
 
-        endpoints = get_neighbouring_enpoints_consumption() # TODO : coder
+        endpoints = self.get_neighbouring_enpoints_consumption() # TODO : coder
         
 
         if self._filter.count > self._threshold:
-            pass
+            endpoints[self._name] = self._local_energy_data.get_avg_cost(arguments)
         else:
+            # TODO : Retirer objectif calculé depuis KNN ou plus cout le plus proche ? 
             pass
+
+        self.distribute_objective(objective, endpoints)
 
     def distribute_objective(objective : float, endpoints_costs : dict[str, interval]):
         pass
 
-    def get_neighbouring_enpoints_consumption() -> dict[str, interval]:
+    def get_neighbouring_enpoints_consumption(self) -> dict[str, interval]:
         """Returns a map containing the consumption of all neighbouring endpoints, keyed by their URL 
 
         Returns:
             dict[str, interval]: a map { URL -> consumption interval }
         """
-        pass
+        dict(map(get_neighbour, self._neighbours))
--- src/App/local_energy_data.py
+++ src/App/local_energy_data.py
@@ -4,12 +4,18 @@ class LocalEnergyData:
     """Caching of latest recorded consumption for a single endpoint
     """
 
-    _args_costs : dict[str, list[float]] = dict()
+    _args_costs : dict[tuple, list[float]] = dict()
     """ the latest recorded consumption of a function and its called arguments. the callee is mapped by his arguments """
 
     def contains_args(self, tuple) -> bool:
         return tuple in self._args_costs
     
+    def register_cost(self, args : tuple, float : float):
+        if args not in self._args_costs:
+            self._args_costs[args] = []
+
+        self._args_costs[args].append(float)
+
     def get_avg_cost(self, tuple) -> float:
         costs = self._args_costs.get(tuple, [])
         
