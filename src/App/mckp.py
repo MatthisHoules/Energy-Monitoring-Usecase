cbff53be93636a5060a5c61ba2e64c6994988eb6
Paul <vautier.paul14350@gmail.com>
2023-05-05 14:23:35 +0200

    working version of MCKP

--- src/App/mckp.py
+++ src/App/mckp.py
@@ -1,25 +1,45 @@
 
 from interval import interval
 from functools import reduce
-import pprint
-def closest_path(intervals : list[interval], objective) -> list[float]:
+
+def closest_path(intervals : list[interval], limit) -> list[float]:
     if not len(intervals):
         return []
     
-    costs_classes : list[list[int]] = [sorted([int(bounds[0]) for bounds in interv]) for interv in intervals]
-    min  = reduce(lambda acc, list : acc + sum(list), costs_classes, 0)
-    if objective < min: 
-        return [v[0] for v in costs_classes]
+    values : list[list[int]] = [sorted([int(bounds[0]) for bounds in interv]) for interv in intervals]
+    min  = reduce(lambda acc, list : acc + list[0], values, 0)
+    if limit <= min: 
+        return [v[0] for v in values]
     
-    dp = [[0 for _ in range(objective + 1)] for _ in range(len(costs_classes))]
-    for i in range(len(costs_classes[0])):
-        if costs_classes[0][i] < objective:
-            dp[0][costs_classes[0][i]] = max(dp[0][costs_classes[0][i]], costs_classes[0][i])
+    dp = [[0 for _ in range(limit + 1)] for _ in range(len(values))]
+    last = [-1] * (limit + 1)
 
-    for i in range(1, len(costs_classes)):
-        for j in range(len(costs_classes[i])):
-            for k in range(costs_classes[i][j], objective + 1):
-                dp[i][k] = max(dp[i][k-1], max(dp[i - 1][k], dp[i - 1][k - costs_classes[i][j]] + costs_classes[i][j]))
-    return 0
+    for i in range(len(values[0])):
+        if values[0][i] < limit:
+            last[values[0][i]] = values[0][i]
+            dp[0][values[0][i]] = values[0][i]
 
-print(closest_path([interval([2, 5], [6, 7]), interval[2, 8], interval[6, 8], interval[4, 2], interval[7, 6]], 4))
+    for i in range(1, len(values)):
+        current = [-1] * (limit + 1)
+        for i_n in values[i]:
+            for w in range(i_n, limit + 1):
+                if last[w - i_n] > 0:
+                    current[w] = max(current[w], last[w - i_n] + i_n)
+                    dp[i][w] = current[w] - last[w - i_n]
+            last = current
+    return find_path_dp(dp)
+
+
+def find_path_dp(dp):
+    y = len(dp) - 1
+    x = len(dp[y]) - 1 - dp[y][::-1].index(max(dp[y]))
+    arr = []
+    while y >= 0:
+        arr.append(dp[y][x])
+        x -= dp[y][x]
+        y-=1
+    arr.reverse()
+    return arr
+v = closest_path([interval([3, 4], [5, 7]), interval[2, 8], interval[6, 8], interval[2, 4], interval[6, 7]], 20)
+
+print(v, sum(v))
